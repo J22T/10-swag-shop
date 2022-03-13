20df863a866a2c128641be0358cbf4bf4bca95a8
J22T <jordan22taylor@gmail.com>
2022-03-12 17:04:24 -0700

    commit

---
+++ js/menuToggle.js
@@ -0,0 +1,8 @@
+//Create a button and give it an ID of "hamburgerBtn"
+//Attach an ID of "primaryNav" to the UL of the unordered list that holds your links
+function toggleMenu() {
+    document.getElementById("hamBTN").classList.toggle("open");
+    document.getElementById("primaryNAV").classList.toggle("open");
+}
+var x = document.getElementById('hamBTN');
+x.onclick = toggleMenu; 
